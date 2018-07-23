d6e90a74f36b5566f8bcaf9c07c84ec88b304ec5
Spencer Thomas <Spencerthomas620@gmail.com>
2018-07-23 09:15:36 -0700

    debuging

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ import jinja2
 import random
 import webapp2
 
-import game-loop
+#import game-loop
 
 jinja_env = jinja2.Environment(
     loader = jinja2.FileSystemLoader(os.path.dirname(__file__))
 )
 
 class MainHandler(webapp2.RequestHandler):
-    get(self):
-        main_template = jinja_env.get_template('main.html')
+    def get(self):
+        main_template = jinja_env.get_template('templates/main.html')
         html = main_template.render(
 
         )
