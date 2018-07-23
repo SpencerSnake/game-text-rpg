bde75f2c3399fc0289fa83ef64e9e70e65ce2ffe
Macks Tillmanns <snnamllit@gmail.com>
2018-07-23 12:08:34 -0400

    Actually imported npcs

--- game_loop.py
+++ game_loop.py
@@ -1,10 +1,7 @@
 # July 23, 2018
 # redesigned-computing-machine
 # the basic game loop
-class BankAccount(object):
-    def __init__(self, label, balance, withdraw, deposit, rename):
-        self.label = label
-        self.balance = balance
-        self.withdraw = withdraw
-        self.deposit = deposit
-        self.rename = rename
+
+from google.appengine.ext import ndb
+
+import npcs
