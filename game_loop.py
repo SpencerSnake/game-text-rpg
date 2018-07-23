d22dee78f865c2fcb61db0d6183db9da8c552822
Josue Dominguez <josued9495@gmail.com>
2018-07-23 16:34:21 -0400

    more combat stuff

--- game_loop.py
+++ game_loop.py
@@ -5,52 +5,55 @@
 from google.appengine.ext import ndb
 
 import npcs
+import random
+
 class Combat(object):
-    enemy = random.choice(enemies) #choose from the enemies list
-    while(#idk what macks wants here =/):
+    def __init__(self, thing1, thing2):#it does matter but it doesn't
+        player = thing1
+        enemy = thing2
 
-        if(player.speed > Enemy.speed): #speed rating decides if either the player or enemy goees first
-            faster = player
-            slower = enemy
+    def damage(self, player1, player2):
+        hit = random.randInt(1,20) + int(player1.intel/7.5)
+        dodge = random.randInt(1,20) + int(player2.speed/7.5) #player 2 is the one BEING hit
+
+        if hit >= dodge:
+            dmg = (((strength + weapon) / (10 - (dex / 3)))/ 1 + (armor / 100))#i dnt knw how to brk it dwn
         else:
-            faster = enemy
-            slower = player
+            print("You missed")
+    def combat_loop(self):
+        while(player.hp > '0' #wait wut):
 
-        if faster = player:
-            #how would we do this part? how do we declare that this person goes first?
-    playerChoice = jinja.FileSystemLoader(#where ever its at)
-    print("Select an option!")
-
-    if player.Choice = fight:
-        #chance to hit
-        if hit = True:
-            print("You have hit" + " " + enemy)
-            #how can we determine how much damage was done?
-            enemy.hp = hp - dmg
-        else:
-            print("You have missed")
-            if enemy.hp <= '0':
-                print("Dead")
+                if(player.speed > Enemy.speed): #speed rating decides if either the player or enemy goees first
+                faster = player
+                slower = enemy
             else:
-                enemy.Attack()
-                #chance to hit
-                if hit = True:
-                    print("The" + enemy + "has hit you")
-                    #how to determine damage
-                    player.hp = hp - dmg
-                else:
-                    print("Enemy has missed")
-                    if player.hp <= '0':
-                        print("GAME OVER")
+                faster = enemy
+                slower = player
 
+            if faster = player:
+                #how would we do this part? how do we declare that this person goes first?
+        playerChoice = jinja.FileSystemLoader(#where ever its at)
+            print("Select an option!")
 
-
-    elif player.Choice = run:
-        quit.Combat
-        #takes me back to where i was before the combat began
-    elif player.Choice = item:
-        print("Select an item!")
-        #actually select the item
-
-    else:
-        #save the game
+            if player.Choice = fight:
+                if hit >= dodge:
+                    dmg
+                    if hit = True:
+                        print("You have hit " + enemy)
+                    #how can we determine how much damage was done?
+                        enemy.hp = hp - dmg
+                    else:
+                        print("You have missed")
+                        if enemy.hp <= '0':
+                            print("Dead")
+                        else:
+                            enemy.Hit()
+                        #chance to hit
+                        if hit = True:
+                            print("The " + enemy + " has hit you")
+                            #how to determine damage
+                            player.hp = hp - dmg
+                        else:
+                            print("Enemy has missed")
+                            if player.hp <= '0':
+                                print("GAME OVER")]]
