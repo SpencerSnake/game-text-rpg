79e935f42af30bf6bc74676592e6669777e3c5db
Macks Tillmanns <snnamllit@gmail.com>
2018-07-24 11:11:11 -0400

    saved

--- npcs.py
+++ npcs.py
@@ -4,8 +4,6 @@
 
 from google.appengine.ext import ndb
 
-<<<<<<< HEAD
-=======
 import webapp2
 
 ### TO DO ###
@@ -14,7 +12,6 @@ import webapp2
 # - simplify damage into the npc class, instead of within damage
 #   step of game_loop
 
->>>>>>> a0b1447c741262a5c926d9c97b86d850b3b7d133
 # This is the base npc class containing all
 # the statistics required for combat. It's
 # used by both players and enemies.
@@ -26,17 +23,11 @@ class npc(ndb.Model):
     # maximum hp
     max_hp = ndb.IntegerProperty(required=True)
     # determines turn order, and dodge chance
-<<<<<<< HEAD
-    speed = ndb.IntegerProperty(required=True)
-    # determines damage reduction, out of 100
-    armor = ndb.IntegerProperty(required=True)
-=======
     speed = ndb.IntegerProperty(required=False)
     # Equipment - determines damage reduction, out of 100
     armor = ndb.KeyProperty(required=True)
     # Equipment - determines base damage
     weapon = ndb.KeyProperty(required=True)
->>>>>>> a0b1447c741262a5c926d9c97b86d850b3b7d133
     # determines base damage
     strength = ndb.IntegerProperty(required=True)
     # determines damage calculation
@@ -44,8 +35,6 @@ class npc(ndb.Model):
     # determines chance to hit
     intel = ndb.IntegerProperty(required=True)
 
-<<<<<<< HEAD
-=======
     def __init__(self):
         self.speed = int(((self.strength*10)/ # Maximum carry weight
             (self.armor.weight+self.weapon.weight))+ # Divided by current carry weight
@@ -81,7 +70,6 @@ class armor(ndb.Model):
 
 
 
->>>>>>> a0b1447c741262a5c926d9c97b86d850b3b7d133
 # This is the player class, used by all users
 # of the game. It contains variables used
 # exclusively in the player encounters.
@@ -93,6 +81,8 @@ class player(npc):
     # player inventory. Stored as string list
     # and parsed.
     inventory = ndb.StringProperty(repeated=True)
+
+
 
 # This is the monster class, used by all
 # all enemies in the game. It contains data
